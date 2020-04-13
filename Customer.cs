4ce8c17cc587f97cca906f499eb962f48e15f42f
Revanth228 <61132235+Revanth228@users.noreply.github.com>
2020-04-13 11:53:25 +0530

    Update Customer.cs

--- Customer.cs
+++ Customer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -9,41 +9,41 @@ namespace Team_1.Models
     public class Customer
     {
         public string Customer_ID { get; set; }
-        [Required(ErrorMessage = "customername is must")]
+        [Required(ErrorMessage = "Customername is must")]
         //[RegularExpression("^[A-Za-z]{1,}$", ErrorMessage = " customername NOT IN CORRECT FORMAT")]
         public string CustomerName { get; set; }
 
-        [Required(ErrorMessage = "Dob is  must")]
+        [Required(ErrorMessage = "Date of birth is  must")]
 
         [CustomDOB(ErrorMessage = "Invalid")]
         public System.DateTime DateOfBirth { get; set; }
-        [RegularExpression("^(?:(?:\\+|0{0,2})91(\\s*[\\-]\\s*)?|[0]?)?[789]\\d{9}$", ErrorMessage = " contactnumber NOT IN CORRECT FORMAT")]
+        [RegularExpression("^(?:(?:\\+|0{0,2})91(\\s*[\\-]\\s*)?|[0]?)?[789]\\d{9}$", ErrorMessage = " Contactnumber not in correct format")]
         //[RegularExpression("^[+]91(789){1}[0-9]{9}$",ErrorMessage ="Contatno is incorrect format!!!")]
 
-        [Required(ErrorMessage = "contactno is must")]
+        [Required(ErrorMessage = "Contactno is must")]
         public long ContactNumber { get; set; }
         [Required(ErrorMessage = "Email is  must")]
         [EmailAddress(ErrorMessage = "Not a valid email")]
         public string EmailAddress { get; set; }
-        [RegularExpression("^[A-Za-z]{1,}$", ErrorMessage = " country NOT IN CORRECT FORMAT")]
-        [Required(ErrorMessage = "country is must")]
+        [RegularExpression("^[A-Za-z]{1,}$", ErrorMessage = " Country not in correct format")]
+        [Required(ErrorMessage = "Country is must")]
         public string Country { get; set; }
-        [RegularExpression("^[A-Za-z]{1,}$", ErrorMessage = " city NOT IN CORRECT FORMAT")]
-        [Required(ErrorMessage = "city is must")]
+        [RegularExpression("^[A-Za-z]{1,}$", ErrorMessage = " City not in correct format")]
+        [Required(ErrorMessage = "City is must")]
         public string City { get; set; }
-        [Required(ErrorMessage = "pincode is must")]
-        [RegularExpression("^[1-9]{1}[0-9]{5}$", ErrorMessage = "pincode NOT IN CORRECT FORMAT")]
+        [Required(ErrorMessage = "Pincode is must")]
+        [RegularExpression("^[1-9]{1}[0-9]{5}$", ErrorMessage = "Pincode not in correct format")]
 
         public int PinCode { get; set; }
-        [Required(ErrorMessage = "state is must")]
+        [Required(ErrorMessage = "State is must")]
         public string State { get; set; }
         public string CustomerType { get; set; }
-        [Required(ErrorMessage = "password is must")]
+        [Required(ErrorMessage = "Password is must")]
 
-        [RegularExpression("^([a-zA-Z0-9@*#$+]{8,25})$", ErrorMessage = "password not in correct format")]
+        [RegularExpression("^([a-zA-Z0-9@*#$+]{8,25})$", ErrorMessage = "Password not in correct format")]
         [Custompass(ErrorMessage = "Invalid")]
         public string Password { get; set; }
-        [Required(ErrorMessage = "conpass is must")]
+        [Required(ErrorMessage = "Confirm password is must")]
         [Compare("Password", ErrorMessage = "Password mismatch")]
         public string Confirmpassword { get; set; }
     }
